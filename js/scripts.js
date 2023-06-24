2d13a9884630eec8a0d032e75794c9b7717aceda
Nick Consolini <99100207+NickDC97@users.noreply.github.com>
2023-06-24 18:29:20 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzPiA2kTEtMnQqtR0Cna9eEjnziW-LoQVR3epx67X7CV55zmT_aeT_-t5voJkj1m0X2Jg/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbw3YHYl5212RZX3mTaSM_AgfRGIrCI_8M39cOEenc9hbCT713GNHXEASHwGdawhM4auIA/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
